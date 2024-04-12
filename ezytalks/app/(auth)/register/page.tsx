5f73bc1532ae1fa4c6fcd720e0b8631a3ac14881
Tanya010802 <91143210+Tanya010802@users.noreply.github.com>
2024-04-12 23:51:09 +0530

    .

---
+++ ezytalks/app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+const RegisterPage = () => {
+    return (
+        <div>
+            <h1>Register Page</h>
+            </div>
+      );
+}
+ 
+export default RegisterPage;
---
+++ ezytalks/app/test/page.tsx
@@ -0,0 +1,10 @@
+const TestPage= () => {
+    return ( 
+        <div>
+            <h1>Test Page</h1>
+            <p>This is a test page. Please do not use this for any real purpose.</p>
+        </div>
+     );
+}
+ 
+export default TestPage ;
